3cbe0be782ade8d365ced1ccb6318217686c9c45
whdrnr <openinhere1@gmail.com>
2022-10-24 10:12:48 +0900

    데인저 함수 구분

--- Assets/Project/02.Script/Manager/GuardBarManager.cs
+++ Assets/Project/02.Script/Manager/GuardBarManager.cs
@@ -5,18 +5,24 @@ using TMPro;
 
 public class GuardBarManager : MonoBehaviour
 {
-    [Header("Unity Editor Version")]
+    [Header("DangerMove1 관련 변수")]
     public float RotateSpeed = 30;
     float RotateZ;
 
     Vector3 Touch_Start;
 
+    [Header("DangerMove2 관련 변수")]
+    Touch Touch;
+
     public GameObject Danger;
 
     void Update()
     {
-        #region #씨발 
-        /*
+        DangerMove2();
+    }
+
+    void DangerMove1()
+    {
         Danger.transform.rotation = Quaternion.Euler(0, 0, RotateZ);
 
         if (Input.GetMouseButtonDown(0))
@@ -26,17 +32,31 @@ public class GuardBarManager : MonoBehaviour
         {
             Vector2 Dir = Touch_Start - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if(Dir.x > 0)
+            if (Dir.x > 0)
+                RotateZ -= RotateSpeed * Time.deltaTime;
+            else
+                RotateZ += RotateSpeed * Time.deltaTime;
+        }
+    }
+
+    void DangerMove2()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch Touch = Input.GetTouch(0);
+
+            if(Touch.phase == TouchPhase.Began)
             {
-                Debug.Log("오른쪽");
+                Debug.Log("Began");
             }
-            else if(Dir.x < 0)
+            if (Touch.phase == TouchPhase.Moved)
             {
-                Debug.Log("왼쪽");
+                Debug.Log("Moved");
+            }
+            if (Touch.phase == TouchPhase.Stationary)
+            {
+                Debug.Log("Stationary");
             }
         }
-        */
-        #endregion
-
     }
 }
