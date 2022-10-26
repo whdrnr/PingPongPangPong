b536c41f6ddeec04b461f2fcc4eb89e126aab0fd
whdrnr <96181722+whdrnr@users.noreply.github.com>
2022-10-27 03:16:51 +0900

    10.27일 3.16분 커밋

--- Assets/Project/02.Script/Controller/GuardBarController.cs
+++ Assets/Project/02.Script/Controller/GuardBarController.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GuardBarController : MonoBehaviour
 {
+    public int Durability = 4;
+
+    public TextMeshProUGUI Durability_Txt;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Pong"))
         {
+            GameManager.Instance.Cur_Socre += 1;
+            UIManager.Instance.PointUp();
+
+            Durability -= 1;
+            Durability_Txt.text = Durability.ToString();
+
+            if(Durability == 0)
+            {
+                GuardDestory();
+            }
         }
+    }
+
+    public void ResetDurability() => Durability = 4;
+
+    void GuardDestory()
+    {
+        Destroy(this.gameObject);
     }
 }
--- Assets/Project/02.Script/Manager/GameManager.cs
+++ Assets/Project/02.Script/Manager/GameManager.cs
@@ -5,15 +5,53 @@ using TMPro;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Header("공 생성 관련 참조")]
     public GameObject Pong;
     public Transform Init_Pos;
-
     public float Speed;
+
+    [Header("데인저 관련 참조")]
+    public GameObject Danger;
+
+    [Header("점수 관련 참조")]
+    public int High_Score = 0;
+    public int Cur_Socre = 0;
+
     public bool IsGame;
 
-    public void PlayGame()
+    public void PlayGame() 
     {
+        UIManager.Instance.Play_Panel.SetActive(true);
+        UIManager.Instance.Lobby_Panel.SetActive(false);
+
+        GameObject CurPong = GameObject.FindWithTag("Pong");
+        CurPong.GetComponent<Rigidbody2D>().velocity = Vector2.down * Speed;
+    }
+
+    public IEnumerator GameOver()
+    {
+        yield return new WaitForSeconds(1f);
+
+        IsGame = false;
+        Danger.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+        //#UI On
+        UIManager.Instance.Play_Panel.SetActive(false);
+        UIManager.Instance.Lobby_Panel.SetActive(true);
+
+        UIManager.Instance.Title_Panel.SetActive(false);
+        UIManager.Instance.Main_Panel.SetActive(true);
+
+        //#점수 관련 참조
+        UIManager.Instance.BeforePoint_Txt.text = Cur_Socre.ToString();
+        UIManager.Instance.CurPoint_Txt.text = "0";
+        Cur_Socre = 0;
+
+        //#공 삭제
+        GameObject CurPong = GameObject.FindWithTag("Pong");
+        Destroy(CurPong);
+
+        //#새로운 공 생성
         GameObject NewPong = Instantiate(Pong, Init_Pos.position, Quaternion.identity);
-        NewPong.GetComponent<Rigidbody2D>().velocity = Vector2.down * Speed;
     }
 }
